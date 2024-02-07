2b044c391cfe4cf0d32dc0b9954de76ce893a332
Andreas Rissling <a.rissling@steinberg.de>
2024-02-07 11:21:26 +0100

    feat: Adjust metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ type RootLayoutProps = Readonly<{ children: React.ReactNode }>;
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Home',
+  title: 'Next React Playground',
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
