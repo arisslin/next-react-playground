e1618357a69e9e8552042d6ab19299b022240c41
Andreas Rissling <a.rissling@steinberg.de>
2024-02-21 10:50:40 +0100

    feat: add onlick logic to toggle switch

--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -1,10 +1,17 @@
-export function ToggleSwitch({
-  children,
-}: Readonly<{ children?: React.ReactNode }>) {
+type ToggleSwitchProps = Readonly<{
+  children?: React.ReactNode;
+  onClick?: () => void;
+}>;
+
+export function ToggleSwitch({ children, onClick }: ToggleSwitchProps) {
   return (
     <label className='flex cursor-pointer	items-center gap-0.5'>
       <span>{children}</span>
-      <input type='checkbox' className='peer h-0 w-0 opacity-0' />
+      <input
+        type='checkbox'
+        onClick={onClick}
+        className='peer h-0 w-0 opacity-0'
+      />
       <div
         data-before=''
         className='relative h-6 w-10 rounded-full bg-slate-400 duration-500 before:absolute before:bottom-0.5 before:left-0.5 before:h-5 before:w-5 before:rounded-full before:bg-slate-50 before:duration-500 before:content-[attr(data-before)] peer-checked:bg-sky-600 peer-checked:before:translate-x-4'
