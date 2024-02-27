d8421cf42c9844af0f7ccc1c0ef2059b23ef8441
Andreas Rissling <a.rissling@steinberg.de>
2024-02-27 10:39:33 +0100

    feat: create heading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,50 @@
+import Heading from '@/components/Heading';
+
 export default function Home() {
-  return <h1 className='container'>Home</h1>;
+  return (
+    <>
+      <Heading order={1}>Heading 1 order</Heading>
+      <p className='container my-4'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem autem
+        odio, quaerat non eos beatae laudantium totam sequi repellat obcaecati
+        molestias quidem enim ducimus possimus libero, impedit quo aperiam
+        iusto.
+      </p>
+      <Heading order={2}>Heading 2 order</Heading>
+      <p className='container my-4'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem autem
+        odio, quaerat non eos beatae laudantium totam sequi repellat obcaecati
+        molestias quidem enim ducimus possimus libero, impedit quo aperiam
+        iusto.
+      </p>
+      <Heading order={3}>Heading 3 order</Heading>
+      <p className='container my-4'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem autem
+        odio, quaerat non eos beatae laudantium totam sequi repellat obcaecati
+        molestias quidem enim ducimus possimus libero, impedit quo aperiam
+        iusto.
+      </p>
+      <Heading order={4}>Heading 4 order</Heading>
+      <p className='container my-4'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem autem
+        odio, quaerat non eos beatae laudantium totam sequi repellat obcaecati
+        molestias quidem enim ducimus possimus libero, impedit quo aperiam
+        iusto.
+      </p>
+      <Heading order={5}>Heading 5 order</Heading>
+      <p className='container my-4'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem autem
+        odio, quaerat non eos beatae laudantium totam sequi repellat obcaecati
+        molestias quidem enim ducimus possimus libero, impedit quo aperiam
+        iusto.
+      </p>
+      <Heading order={6}>Heading 6 order</Heading>
+      <p className='container my-4'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem autem
+        odio, quaerat non eos beatae laudantium totam sequi repellat obcaecati
+        molestias quidem enim ducimus possimus libero, impedit quo aperiam
+        iusto.
+      </p>
+    </>
+  );
 }
---
+++ src/components/Heading.tsx
@@ -0,0 +1,46 @@
+type HeadingProps = {
+  order: 1 | 2 | 3 | 4 | 5 | 6;
+  children: React.ReactNode;
+};
+
+export default function Heading({
+  order,
+  children,
+}: Readonly<HeadingProps>): JSX.Element | null {
+  const commonClassName = 'container font-bold';
+
+  const headings = {
+    1: (
+      <h1 className={`${commonClassName} my-9 text-4xl md:my-12 md:text-5xl`}>
+        {children}
+      </h1>
+    ),
+    2: (
+      <h2 className={`${commonClassName} my-7 text-3xl md:my-9 md:text-4xl`}>
+        {children}
+      </h2>
+    ),
+    3: (
+      <h3 className={`${commonClassName} my-6 text-2xl md:my-7 md:text-3xl`}>
+        {children}
+      </h3>
+    ),
+    4: (
+      <h4 className={`${commonClassName} my-5 text-xl md:my-6 md:text-2xl`}>
+        {children}
+      </h4>
+    ),
+    5: (
+      <h5 className={`${commonClassName} my-5 text-lg md:text-xl`}>
+        {children}
+      </h5>
+    ),
+    6: (
+      <h6 className={`${commonClassName} my-4 md:my-5 md:text-lg`}>
+        {children}
+      </h6>
+    ),
+  };
+
+  return headings[order] ?? null;
+}
