b7795a64836f54f87524d8118095c93fcfab2896
Andreas Rissling <a.rissling@steinberg.de>
2024-02-29 16:06:23 +0100

    feat: create card link component

---
+++ src/components/CardLink.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+export type CardLinkProps = { href: string; title: string; subtext?: string };
+
+export function CardLink({ href, title, subtext }: Readonly<CardLinkProps>) {
+  return (
+    <Link
+      href={href}
+      className='group block rounded-md border border-slate-500 p-4 hover:bg-slate-500 dark:border-slate-700 dark:bg-slate-700 dark:hover:border-slate-400 dark:hover:bg-slate-500'
+    >
+      <h4 className='text-lg font-bold text-sky-700 group-hover:text-sky-300 dark:text-sky-200'>
+        {title}
+      </h4>
+      {subtext ? (
+        <p className='font-light group-hover:text-gray-50'>{subtext}</p>
+      ) : null}
+    </Link>
+  );
+}
