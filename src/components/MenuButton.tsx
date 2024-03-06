30ea1d40f7af867fd5d982cb2487e822767e5e8f
Andreas Rissling <a.rissling@steinberg.de>
2024-03-06 18:09:47 +0100

    feat: style menu button for dark mode

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -9,12 +9,12 @@ export function MenuButton({
 }: Readonly<MenuButtonProps>): JSX.Element {
   return (
     <button
-      className={`p-4 hover:bg-sky-200${className ? ' ' + className : ''}`}
+      className={`p-4 hover:bg-sky-200 dark:hover:bg-sky-500${className ? ' ' + className : ''}`}
       onClick={onClick}
     >
-      <div className='h-0.5 w-4 bg-gray-950'></div>
-      <div className='my-0.5 h-0.5 w-4 bg-gray-950'></div>
-      <div className='h-0.5 w-4 bg-gray-950'></div>
+      <div className='h-0.5 w-4 bg-gray-950 dark:bg-slate-50'></div>
+      <div className='my-0.5 h-0.5 w-4 bg-gray-950 dark:bg-slate-50'></div>
+      <div className='h-0.5 w-4 bg-gray-950 dark:bg-slate-50'></div>
     </button>
   );
 }
