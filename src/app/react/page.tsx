1eb075d4e884f473c32d5d011651e3399c752f8d
Andreas Rissling <a.rissling@steinberg.de>
2024-03-08 10:54:01 +0100

    feat: add first content on react route

--- src/app/react/page.tsx
+++ src/app/react/page.tsx
@@ -1,3 +1,21 @@
-export default function ReactPage() {
-  return <h1>React</h1>;
+import Heading from '@/components/Heading';
+
+export default function ReactPage(): JSX.Element {
+  return (
+    <>
+      <Heading order={1}>React.js</Heading>
+      <p className='container'>
+        Here you can find some examples of react.js. They are based on the
+        reference on the official{' '}
+        <a
+          href='https://react.dev/'
+          target='_blank'
+          className='text-sky-600 underline hover:text-sky-500 dark:text-sky-300 dark:hover:text-sky-400'
+        >
+          React.js website
+        </a>
+        .
+      </p>
+    </>
+  );
 }
