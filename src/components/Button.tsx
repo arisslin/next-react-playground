52c0079ee4241e0ba1837b4a05f9b7f0c9d11be6
Andreas Rissling <a.rissling@steinberg.de>
2024-03-12 16:42:56 +0100

    feat: add disbale logic to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,15 @@
 type ButtonProps = {
   children: React.ReactNode;
   onClick?: () => void;
+  disabled?: boolean;
 };
 
-export function Button({ children, onClick }: Readonly<ButtonProps>) {
+export function Button({ children, onClick, disabled }: Readonly<ButtonProps>) {
   return (
     <button
       onClick={onClick}
-      className='cursor rounded bg-sky-500 px-3 py-2 text-gray-50 shadow-md active:shadow-none'
+      className='cursor rounded bg-sky-500 px-3 py-2 text-gray-50 shadow-md active:shadow-none disabled:bg-gray-400 disabled:shadow-none'
+      disabled={disabled}
     >
       {children}
     </button>
