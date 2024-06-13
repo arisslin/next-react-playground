e242fd9de518db337d2e89b66c3daa429ad4d81e
Andreas Rissling <a.rissling@steinberg.de>
2024-06-13 10:10:46 +0200

    fix: dark text color in card link

--- src/app/react/useState/page.tsx
+++ src/app/react/useState/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from '@/components/Button';
+import { Button } from '@/components/button/Button';
 import { Heading } from '@/components/heading/Heading';
 import { useState } from 'react';
 import { CodeBlock } from '@/components/codeblock/TypeScriptCodeBlock';
--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -1,7 +1,20 @@
+import classNames from 'classnames';
+
+type ParagraphProps = Readonly<{
+  children: React.ReactNode;
+  optionalClassName?: string;
+  hasContainer?: boolean;
+}>;
+
 export default function Paragraph({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <p className='container my-4 text-gray-950 dark:text-gray-50'>{children}</p>
+  optionalClassName,
+  hasContainer = true,
+}: ParagraphProps): JSX.Element {
+  const className = classNames(
+    { container: hasContainer, 'my-4': hasContainer },
+    'text-gray-950 dark:text-gray-50',
+    optionalClassName
   );
+  return <p className={className}>{children}</p>;
 }
