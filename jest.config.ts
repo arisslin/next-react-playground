207c7c6e8182d512d5d4ef104dc5a4c432f3e839
Andreas Rissling <a.rissling@steinberg.de>
2024-07-08 17:22:14 +0200

    feat: config jest

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  moduleNameMapper: {
+    // ...
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);
